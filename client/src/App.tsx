7bc52fd2fdb0ea723b8098145e21e36ac8e926c9
mait21606-coder <mait21606@gmail.com>
2026-06-07 00:02:48 +0700

    Cập nhật câu hỏi

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import MainLayout from './layouts/MainLayout';
+import MainLayout from './layouts/MainLayouts';
+import Home from './pages/Home';
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Định nghĩa Layout chung cho toàn bộ website */}
         <Route path="/" element={<MainLayout />}>
-          
-          {/* Hiện tại bên trong đang rỗng, chưa có trang nào */}
-          <Route index element={<div>Trang chủ đang được phát triển...</div>} />
-          
-          {/* Sau này các bạn khác chỉ cần thêm các Route con vào đây */}
-          {/* Ví dụ: <Route path="profile" element={<Profile />} /> */}
-          
+          <Route index element={<Home />} />
         </Route>
       </Routes>
     </BrowserRouter>
