7ff066487d4a99d3c6deff954d27b4ee18ce9ed2
namm2305 <tanhatnam2006@gmail.com>
2026-06-08 07:14:44 +0700

    final  fix

--- server/index.js
+++ server/index.js
@@ -11,13 +11,7 @@ const app = express();
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors({
-    origin: function (origin, callback) {
-        // Cho phép mọi domain (bao gồm Netlify) kết nối tới backend
-        callback(null, true);
-    },
-    credentials: true
-}));
+app.use(cors());
 
 app.use('/api/auth', require('./routes/auth'));
 
